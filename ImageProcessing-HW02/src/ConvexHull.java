cde8857dcc831891a317d39d2e5913a8232b78a6
nburakozgul <brk_ozgul@hotmail.com>
2017-03-13 22:20:57 +0300

    convex hull done

--- ImageProcessing-HW02/src/ConvexHull.java
+++ ImageProcessing-HW02/src/ConvexHull.java
@@ -1,31 +1,66 @@
 import vpt.Image;
+import vpt.algorithms.display.Display2D;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by brk_o on 3/12/2017.
  */
 public class ConvexHull {
 
+    private Image createOneChannelImage(Image input){
+        boolean hit = true;
+        Image resultImg = input.newInstance(input.getXDim(), input.getYDim(),1);
+
+        for(int y = 0 ; y < input.getYDim() ; y++) {
+            for(int x = 0; x < input.getXDim() ; x++) {
+                for (int c = 0 ; c < input.getCDim(); c++)  {
+                    if (input.getXYCByte(x, y, c) == 0) {
+                        hit = false;
+                        break;
+                    }
+                }
+
+                if (hit)
+                    resultImg.setXYByte(x, y, 255);
+                else
+                    resultImg.setXYByte(x, y, 0);
+
+                hit = true;
+            }
+        }
+        return resultImg;
+    }
+
     private Image applyFilter(Image input, int[][] filter ) {
-        Image resultImg = input.newInstance(input.getXDim(), input.getYDim(),input.getCDim());
+        Image resultImg = input.newInstance(false);
         boolean hit = true;
         int filterValue;
         int imageValue;
 
-        for(int y = 1 ; y < input.getYDim() - 1 ; y++) {
-            for(int x = 1; x < input.getXDim() - 1 ; x++) {
+
+        for(int y = 1 ; y < input.getYDim() -1 ; y++) {
+            for(int x = 1; x < input.getXDim() -1 ; x++) {
 
                 for(int j = -1 ; j < 2 ; j++) {
                     for (int i = -1 ; i < 2 ; i++) {
                         imageValue = input.getXYByte(x + i, y + j);
                         filterValue = filter[j+1][i+1];
 
-                        if (filterValue == 1 && imageValue != 255) {
+                        if (filterValue == 0)
+                            continue;
+                        else if (filterValue == 1 && imageValue != 255) {
                             hit = false;
+                            break;
                         } else if (filterValue == -1 && imageValue != 0) {
                             hit = false;
-                        } else if (filterValue == 0)
-                            continue;
+                            break;
+                        }
                     }
+
+                    if (!hit)
+                        break;
                 }
 
                 if (hit)
@@ -40,7 +75,84 @@ public class ConvexHull {
         return resultImg;
     }
 
-    public Image applyFilterPublic(Image img, int[][] filter) {
-        return applyFilter( img , filter );
+    private Image applyFilterMultipleTime(Image input, int[][] filter) {
+        input = createOneChannelImage(input);
+        Image resultImage;
+        Image inputCopy = input;
+
+        resultImage = applyFilter(inputCopy,filter);
+
+        while(!this.equal(resultImage,inputCopy)) {
+            inputCopy = resultImage;
+            resultImage = applyFilter(inputCopy,filter);
+        }
+
+        return resultImage;
+    }
+
+    private boolean equal(Image img1, Image img2) {
+        if (img1.getXDim() != img2.getXDim())
+            return false;
+        if (img1.getYDim() != img2.getYDim())
+            return false;
+
+        for(int y = 0 ; y < img1.getYDim(); y++)
+            for(int x = 0 ; x < img1.getXDim(); x++) {
+                if (img1.getXYByte(x,y) != img2.getXYByte(x,y))
+                    return false;
+            }
+        return true;
+    }
+
+    private Image conbineImages(List<Image> imageList) {
+        Image resultImage = imageList.get(0).newInstance(false);
+        int H = imageList.get(0).getYDim();
+        int W = imageList.get(0).getXDim();
+
+        boolean hit = false;
+
+        for (int y = 0 ; y < H ; y++) {
+            for (int x  = 0 ; x < W ; x++) {
+
+                for (Image img : imageList) {
+                    if (img.getXYByte(x, y) == 255) {
+                        hit = true;
+                        break;
+                    }
+                }
+
+                if (hit) {
+                    resultImage.setXYByte(x, y, 255);
+                    hit = false;
+                } else {
+                    resultImage.setXYByte(x, y, 0);
+                }
+            }
+        }
+
+        return resultImage;
+    }
+
+    public Image convexHull(Image input) {
+        input = createOneChannelImage(input);
+        List<Image> imageList = new ArrayList<Image>();
+
+        int[][] filter = {{1,0,0},{1,-1,0},{1,0,0}};
+        Image resultImage = applyFilterMultipleTime(input, filter);
+        imageList.add(resultImage);
+
+        filter = new int[][]{{1, 1, 1}, {0, -1, 0}, {0, 0, 0}};
+        resultImage = applyFilterMultipleTime(input, filter);
+        imageList.add(resultImage);
+
+        filter = new int[][]{{0, 0, 1}, {0, -1, 1}, {0, 0, 1}};
+        resultImage = applyFilterMultipleTime(input, filter);
+        imageList.add(resultImage);
+
+        filter = new int[][]{{0, 0, 0}, {0, -1, 0}, {1, 1, 1}};
+        resultImage = applyFilterMultipleTime(input, filter);
+        imageList.add(resultImage);
+
+        return conbineImages(imageList);
     }
 }
--- ImageProcessing-HW02/src/Main.java
+++ ImageProcessing-HW02/src/Main.java
@@ -5,19 +5,8 @@ import vpt.algorithms.io.Load;
 public class Main {
     public static void main(String[] args) {
         ConvexHull CH = new ConvexHull();
-        int[][] filter = {{0,0,0},{1,-1,-1},{0,0,0}};
-
-
-        // Goruntu belgesini diskten bellege aktar
         Image img = Load.invoke("perfectly_painted_cat.png");
-
-        // goruntule
         Display2D.invoke(img);
-
-        // 100, 100 konumuna yeni deger ata
-        img = CH.applyFilterPublic(img, filter);
-
-        // yeniden goruntule
-        Display2D.invoke(img);
+        Display2D.invoke(CH.convexHull(img));
     }
 }
